3075cad1685c1ae7ddbd2e560971e361c8650e90
Chrisando Ryan <chrisando.siahaan@gmail.com>
2022-06-16 17:01:52 +0700

    display agenda from state to table
    - display agenda from state to table component
    - add component lifecycle and

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Col, Container, Nav, Navbar, NavDropdown, Row, Table } from 'react-bootstrap';
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -7,23 +7,51 @@ class HomePage extends React.Component {
         this.state = {
             agendas: [
                 {
-                    title: "Agenda 1",
-                    date: "",
-                    description: ""
+                    title: "Belajar React JS",
+                    date: "17 Juni 2022",
+                    description: "Belajar React JS biar bisa bikin web untuk nge-hack NASA"
                 },
                 {
-                    title: "Agenda 2",
-                    date: "",
-                    description: ""
+                    title: "Makan Baso Boedjangan",
+                    date: "16 Juni 2022",
+                    description: "Kumps bersama teman-teman"
                 },
                 {
-                    title: "Agenda3",
-                    date: "",
-                    description: ""
-                }
+                    title: "Nonton Konser",
+                    date: "31 Juni 2022",
+                    description: "Hiling"
+                },
+                {
+                    title: "Beli Rumah",
+                    date: "31 Juni 2023",
+                    description: "Modal Usaha"
+                },
             ],
             pageTitle: "Agenda - Homepage",
         }
+    }
+
+    componentDidMount() {
+        // setelah 5 detik, data agenda bertambah dan masuk ke state agendas
+        setTimeout(() => {
+            // let oldAgendas = this.state.agendas; // gaboleh gini, karena copy reference
+            let oldAgendas = [...this.state.agendas]; // copy isi this.state.agendas ke variable baru
+
+            oldAgendas.push({
+                title: "Kerjain PR",
+                date: "20 Juni 2022",
+                description: "Biar ga dimarahin Bu Guru"
+            });
+            this.setState({
+                agendas: oldAgendas
+            });
+        }, 5000);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("Component is updated!");
+        console.log("Prev Props", prevProps);
+        console.log("Prev State", prevState);
     }
 
     // dimana kita menulis HTML agar dirender oleh browser
@@ -55,6 +83,45 @@ class HomePage extends React.Component {
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
+                </div>
+                {/* Content */}
+                <div>
+                    <Container>
+                        <Row>
+                            {/* Untuk Today's Agenda */}
+                            <Col style={{ border: "1px solid black" }} xs={3} md={3} lg={3} >
+                                1
+                            </Col>
+                            {/* Untuk Table Agenda */}
+                            <Col style={{ border: "1px solid black" }} xs={9} md={9} lg={9}>
+                                <h3>Your Agenda</h3>
+                                <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Title</th>
+                                            <th>Date</th>
+                                            <th>Description</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            this.state.agendas.map((agenda, i) => {
+                                                return (
+                                                    <tr>
+                                                        <td>{i + 1}</td>
+                                                        <td>{agenda.title}</td>
+                                                        <td>{agenda.date}</td>
+                                                        <td>{agenda.description}</td>
+                                                    </tr>
+                                                )
+                                           }) 
+                                        }
+                                    </tbody>
+                                </Table>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
             </div>
         )
