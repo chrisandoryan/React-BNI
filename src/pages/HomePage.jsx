37af984d047cef4fcb18290dc03de89e48877ffa
Chrisando Ryan <chrisando.siahaan@gmail.com>
2022-06-16 15:51:11 +0700

    remove name and address from HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -33,25 +33,23 @@ class HomePage extends React.Component {
                 {/* Heading/Judul */}
                 <div>
                     <h1>{this.state.pageTitle}</h1>
-                    <h2>Nama saya {this.props.userName}</h2>
-                    <h2>Saya tinggal di {this.props.userAddress}</h2>
+                    {/* <h2>Nama saya {this.props.userName}</h2>
+                    <h2>Saya tinggal di {this.props.userAddress}</h2> */}
                 </div>
                 {/* Navbar */}
                 <div>
                     <Navbar bg="light" expand="lg">
                         <Container>
-                            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                            <Navbar.Brand href="#home">Ultra React Agenda</Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
                                 <Nav className="me-auto">
                                     <Nav.Link href="#home">Home</Nav.Link>
-                                    <Nav.Link href="#link">Link</Nav.Link>
-                                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                    <NavDropdown title="Menu" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#action/3.1">Add Agenda</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2">View Agenda History</NavDropdown.Item>
                                         <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
                                     </NavDropdown>
                                 </Nav>
                             </Navbar.Collapse>
